959c67952d12a4fe73e999a099380e60cc85025e
Limbo2Forma <nam671999hai@gmail.com>
2019-08-01 17:42:21 +0700

    Add SQL stored procedure files

---
+++ Prototype/SQL/dbo.GetEventDetailFromEventID.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE GetEventDetailFromEventID (
+	@EventID INT
+)
+AS
+BEGIN
+	SELECT * FROM dbo.EventDetail 
+	WHERE dbo.Event.ID = @EventID
+	AND dbo.Event.ID = dbo.EventDetail.EventID
+END
---
+++ Prototype/SQL/dbo.GetEventFromCompanyCodeAndStartDate.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE GetEventFromCompanyCodeAndStartDate (
+		@CompanyCode varchar(10), -- the companycode of the company whom data is to be returned
+		@Date datetime = null -- The date from 
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SELECT * FROM dbo.Event 
+	WHERE CompanyCode = @CompanyCode 
+	AND DATEDIFF(hour,@Date,StartDate) < 24
+	AND DATEDIFF(hour,@Date,StartDate) > 0
+	ORDER BY StartDate
+END
